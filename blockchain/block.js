83bd751c46fa43cd2514b8d82eb685227c725c3f
Kurt Schneider <k.w.schneid@gmail.com>
2018-06-23 12:25:28 -0400

    Dynamically adjust difficulty

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,36 +1,46 @@
 // import hashing function
 const SHA256 = require('crypto-js/sha256');
-const { DIFFICULTY } = require('../config');
+const { DIFFICULTY, MINE_RATE } = require('../config');
 
 // 4 main parts of a block on a blockchain - the time of creation, the hash from the last block in the blockchain, the hash, and the data. Use class to make multiple instances
 class Block {
 	// set initial values
-	constructor(timestamp, lastHash, hash, data, nonce) {
+	// timestamp - time when block was created
+	// lastHash - hash of previous block in blockchain
+	// hash - unique identifier for block, made from other parameters and hashing function
+	// data - data we want to be associated with the block
+	// nonce - number of leading 0's
+	// difficulty - how hard it is to mine block, changes dynamically, for first block set to DIFFICULTY
+	constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
 		this.timestamp = timestamp;
 		this.lastHash = lastHash;
 		this.hash = hash;
 		this.data = data;
 		this.nonce = nonce;
+		this.difficulty = difficulty || DIFFICULTY;
 	}
 
 	// Make it so we can see in console
 	toString() {
 		return `Block -
-			Timestamp:${this.timestamp}
-			Last Hash:${this.lastHash.substring(0, 10)}
-			Hash     :${this.hash.substring(0, 10)}
-			Nonce    :${this.nonce}
-			Data     :${this.data}`;
+			Timestamp  :${this.timestamp}
+			Last Hash  :${this.lastHash.substring(0, 10)}
+			Hash       :${this.hash.substring(0, 10)}
+			Nonce      :${this.nonce}
+			Difficulty : ${this.difficulty}
+			Data       :${this.data}`;
 	}
 	
 	// initial block on blockchain
+	// Static method calls are made directly on the class and are not callable on instances of the class.
 	static genesis() {
-		return new this('Genesis time', '------', 'f1r57-h45h', [], 0);
+		return new this('Genesis time', '------', 'f1r57-h45h', [], 0, DIFFICULTY);
 	}
 
 	static mineBlock(lastBlock, data) {
 		let hash, timestamp;
 		const lastHash = lastBlock.hash;
+		let { difficulty } = lastBlock;
 		let nonce = 0;
 
 		// increase nonce while it doesn't match the difficulty,
@@ -39,25 +49,36 @@ class Block {
 		do {
 			nonce ++;
 			timestamp = Date.now();
-			hash = Block.hash(timestamp, lastHash, data, nonce);
+			difficulty = Block.adjustDifficulty(lastBlock, timestamp);
+			hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
 
-			// substring gets the first 0 - DIFFICULTY values of the hash to check how many 0's there are
-		} while(hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+			// substring gets the first 0 - difficulty values of the hash to check how many 0's there are
+			// difficulty is defined dynamically 
+		} while(hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 		
 
-		return new this(timestamp, lastHash, hash, data, nonce);
+		return new this(timestamp, lastHash, hash, data, nonce, difficulty);
 	}
  
  	// make a hash for this block using inputs and imported SHA256 module. 
-	static hash(timestamp, lastHash, data, nonce) {
-		return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
+	static hash(timestamp, lastHash, data, nonce, difficulty) {
+		return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
 	}
 
 	// lets individual block generate a hash
 	static blockHash(block) {
-		const { timestamp, lastHash, data, nonce } = block;
+		const { timestamp, lastHash, data, nonce, difficulty } = block;
 
-		return Block.hash(timestamp, lastHash, data, nonce);
+		return Block.hash(timestamp, lastHash, data, nonce, difficulty);
+	}
+
+	static adjustDifficulty(lastBlock, currentTime) {
+		let { difficulty } = lastBlock;
+		difficulty = lastBlock.timestamp + MINE_RATE > currentTime 
+			? difficulty + 1
+			: difficulty - 1;
+			
+		return difficulty;
 	}
 }
 
--- config.js
+++ config.js
@@ -3,6 +3,14 @@
 // to match the current difficulty of blockchain
 // the more miners there are the higher the difficulty, to keep the rate of adding
 // blocks to the blockchain consistent (i.e. 10 mins for bitcoin)
-const DIFFICULTY = 4;
+const DIFFICULTY = 3;
 
-module.exports = { DIFFICULTY };
+// Dynamic mining rate
+// To make dynamic mine rate we should compare the timestamps for mining blocks
+// if it is mined too quickly, we need to increase the difficulty
+// if it is mined too slowly, we need to decrease the difficulty
+// We should always strive to reach a set interval for block mining
+
+const MINE_RATE = 3000;
+
+module.exports = { DIFFICULTY, MINE_RATE };
