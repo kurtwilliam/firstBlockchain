37414cbb9f5cd7cde3641b58144a21a4f4afa196
Kurt Schneider <k.w.schneid@gmail.com>
2018-07-03 14:49:08 -0400

    created transactions endpoint

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 // Use port 3001 if other one isn't available.
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -24,6 +28,10 @@ app.post('/mine', (req, res) => {
 	p2pServer.syncChains();
 
 	res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+	res.json(tp.transactions);
 });
 
 app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
